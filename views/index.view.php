48c93af2361cfaa5ec13636822db92ed9f549feb
Evelyn Hernández Martínez <106703825+evelyneby@users.noreply.github.com>
2023-12-09 22:54:16 -0600

    Add files via upload

---
+++ views/index.view.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Galeria Paisajes</title>
+    
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+<head>
+    <div class="contenedor">
+    <h1 class="titulo">Mi Galeria de Paisajes en PHP y MySql</h1>
+    </div>
+<section class="fotos">
+    <div class="contenedor">
+        <dir ></dir>
+
+    </div>
+<div class="contenedor">
+        <dir class="thumb">
+            <a href="#">
+            <img src="imagenes/1.jpg">
+			<img src="imagenes/2.jpg">
+			<img src="imagenes/3.jpg">
+			<img src="imagenes/4.jpg">
+			<img src="imagenes/5.jpg">
+			<img src="imagenes/6.jpg">
+			<img src="imagenes/7.jpg">
+			<img src="imagenes/8.jpg">
+			
+        </a>
+        </dir>
+    </div>
+</section>
+</head>
+</body>
+</html>
